0584d71a6370e4241a651aee11dd942f661907eb
Bart <bartd@medicinemen.eu>
2022-12-02 21:11:11 +0100

    initial commit

---
+++ schemescope/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to schemescope/schemescope
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ schemescope/test/schemescope/schemescope_test.clj
@@ -0,0 +1,7 @@
+(ns schemescope.schemescope-test
+  (:require [clojure.test :refer :all]
+            [schemescope.schemescope :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
